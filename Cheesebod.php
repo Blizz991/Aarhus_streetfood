a1e6f75626bd531626cbe9331cafc19be1dc240c
Helenawitte1 <h-hwitte@hotmail.com>
2018-11-08 14:51:47 +0100

    Madboder
    Her er der lavet madboderne, mangler den sidste

---
+++ Cheesebod.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include("includes/headerScripts.php"); ?>
+    <title>Aarhus Streetfood</title>
+</head>
+<body>
+<?php include("includes/mainNav.php"); ?>
+<div class="container">
+    
+<img src="images/Cheese.jpg" class="img-responsive">   
+    
+    
+<h2 class="overskrift_boder"> Grilled Cheese </h2>
+    <h4>Smeltet ost i rigelige mængder - behøver vi sige mere?</h4>
+ 
+<p> Grilled cheese laver sandwiches med ost - lækker blød varm smeltet ost - og friskt bagerbrød, confit de canard, krydret pølse, svampe og stegt aubergine. 
+    Det er lækre og veltillavede ingredienser lukket inde i en mundvandsfremkaldende lomme af ost og brød. Perfekt til at kurere tømmermændene, til at tage sulten, til at runde aftenen af med, til at....ja, faktisk perfekt til alt!</p> 
+    
+    <h3 class=overskrift_boder> Menukort</h3>
+
+    <table style="width:100%">
+ 
+  <tr>
+    <td>Grilled toast</td>
+    <td>45 kr.</td> 
+        </tr>
+<tr>
+<td> Cheese fries </td>  
+<td> 35 kr. </td>    
+</tr>
+        
+<tr>
+    <td>Cheese kylling </td>
+    <td>65 kr.</td>
+</tr>   
+<tr>        
+    <td>Syltede løg med ost</td>
+    <td>25 kr. </td> 
+        </tr>    
+        <tr> 
+    <td>Chili Cheese tops</td>  
+ <td>45 kr.</td>
+        </tr>
+        
+<tr>        
+    <td>Cheese bread</td>
+    <td>30 kr.</td>
+  </tr>
+</table>
+    
+
+</div>
+<?php include("includes/footer.php"); ?>
+
+</body>
+<?php include("includes/afterBodyScripts.php"); ?>
+
+</html>
