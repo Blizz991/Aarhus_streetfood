da0307f47e484434b730c47b89d01512e6cd6342
Martin Høite <eaamhoe@students.eaaa.dk>
2018-11-09 13:31:43 +0100

    Setup footer (still needs media queries for the social media container - as it's currently right aligned).

--- index.php
+++ index.php
@@ -8,14 +8,11 @@
 <?php include("includes/mainNav.php"); ?>
 <div class="container">
 
-
-
     <!-- SnapWidget for instagram integration -->
     <script src="https://snapwidget.com/js/snapwidget.js"></script>
     <iframe src="https://snapwidget.com/embed/619604" class="snapwidget-widget" allowtransparency="true" frameborder="0" scrolling="no" style="border:none; overflow:hidden; width:100%; "></iframe>
 </div>
 <?php include("includes/footer.php"); ?>
-
 </body>
 <?php include("includes/afterBodyScripts.php"); ?>
 
