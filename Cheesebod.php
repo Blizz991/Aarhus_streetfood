8e20c0118e00789855fded54b739fa4eab517ae8
Helenawitte1 <h-hwitte@hotmail.com>
2018-11-14 12:24:56 +0100

    Nye billeder

--- Cheesebod.php
+++ Cheesebod.php
@@ -2,58 +2,58 @@
 <html lang="en">
 <head>
     <!--Meta tag for Grilled Cheese-->
-    
+
     <meta name="description" content="Grilled cheese laver sandwiches med ost i friskt bagerbrød med confit de canard, krydret pølse, svampe og stegt aubergine. ">
-    
+
     <?php include("includes/headerScripts.php"); ?>
     <title>Aarhus Streetfood</title>
 </head>
 <body>
 <?php include("includes/mainNav.php"); ?>
 <div class="container">
-    
-<img src="images/Seperate_boder/GrilledCheese_GrilledCheese.jpg" class="img-responsive" alt="Grilled Cheese" witdh="100%">
-    
-    
-<h2 class="overskrift_boder"> Grilled Cheese </h2>
-    <h4>Smeltet ost i rigelige mængder - behøver vi sige mere?</h4>
- 
-<p> Grilled cheese laver sandwiches med ost - lækker blød varm smeltet ost - og friskt bagerbrød, confit de canard, krydret pølse, svampe og stegt aubergine. 
-    Det er lækre og veltillavede ingredienser lukket inde i en mundvandsfremkaldende lomme af ost og brød. Perfekt til at kurere tømmermændene, til at tage sulten, til at runde aftenen af med, til at....ja, faktisk perfekt til alt!</p> 
-    
-    <h3 class=overskrift_boder> Menukort</h3>
+    <h1 class="overskrift_boder"> Grilled Cheese </h1>
+    <div class="bod-container">
+        <div class="bod-info-container">
+            <img src="images/Seperate_boder/GrilledCheese_GrilledCheese.jpg" class="img-responsive" alt="Grilled Cheese">
+            <h4>Smeltet ost i rigelige mængder - behøver vi sige mere?</h4>
+             
+            <p> Grilled cheese laver sandwiches med ost - lækker blød varm smeltet ost - og friskt bagerbrød, confit de canard, krydret pølse, svampe og stegt aubergine. 
+                Det er lækre og veltillavede ingredienser lukket inde i en mundvandsfremkaldende lomme af ost og brød. Perfekt til at kurere tømmermændene, til at tage sulten, til at runde aftenen af med, til at....ja, faktisk perfekt til alt!</p>
+        </div>
+        <div class="menu-table-container">
+            <h3 class="menu-title"> Menukort</h3>
 
-    <table style="width:100%">
- 
-  <tr>
-    <td>Grilled toast</td>
-    <td>45 kr.</td> 
-        </tr>
-<tr>
-<td> Cheese fries </td>  
-<td> 35 kr. </td>    
-</tr>
-        
-<tr>
-    <td>Cheese kylling </td>
-    <td>65 kr.</td>
-</tr>   
-<tr>        
-    <td>Syltede løg med ost</td>
-    <td>25 kr. </td> 
-        </tr>    
-        <tr> 
-    <td>Chili Cheese tops</td>  
- <td>45 kr.</td>
-        </tr>
-        
-<tr>        
-    <td>Cheese bread</td>
-    <td>30 kr.</td>
-  </tr>
-</table>
-    
+            <table cellpadding="10" class="menu-table">
 
+                <tr>
+                    <td>Grilled toast</td>
+                    <td>45 kr.</td>
+                </tr>
+                <tr>
+                    <td> Cheese fries</td>
+                    <td> 35 kr.</td>
+                </tr>
+
+                <tr>
+                    <td>Cheese kylling</td>
+                    <td>65 kr.</td>
+                </tr>
+                <tr>
+                    <td>Syltede løg med ost</td>
+                    <td>25 kr.</td>
+                </tr>
+                <tr>
+                    <td>Chili Cheese tops</td>
+                    <td>45 kr.</td>
+                </tr>
+
+                <tr>
+                    <td>Cheese bread</td>
+                    <td>30 kr.</td>
+                </tr>
+            </table>
+        </div>
+    </div>
 </div>
 <?php include("includes/footer.php"); ?>
 
